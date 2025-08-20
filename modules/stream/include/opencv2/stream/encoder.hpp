09a3fec09728be7b0fa59859245cbb164607e06c
CodeHotel <schooldog07@dgu.ac.kr>
2025-08-20 20:58:48 +0000

    basic header includes

--- modules/stream/include/opencv2/stream/encoder.hpp
+++ modules/stream/include/opencv2/stream/encoder.hpp
@@ -1,4 +1,13 @@
 #ifndef OPENCV_ENCODER_HPP
 #define OPENCV_ENCODER_HPP
 
+#include <opencv2/core.hpp>
+
+extern "C" {
+#include <libavcodec/avcodec.h>
+#include <libswscale/swscale.h>
+#include <libavutil/frame.h>
+#include <libavutil/imgutils.h>
+}
+
 #endif //OPENCV_ENCODER_HPP
